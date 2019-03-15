17c102b833748219df269f85284dc70b5c57fca8
Tomek <t.klepacki@wp.pl>
2019-03-15 21:43:45 +0100

    feature/ZADANIE-7 - adding fixes

--- src/test/java/mohito/de/LoginTest.java
+++ src/test/java/mohito/de/LoginTest.java
@@ -5,7 +5,6 @@ import static org.testng.Assert.assertTrue;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import driver.WebDriverCreator;
