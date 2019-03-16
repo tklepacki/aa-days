a48418a39de358310f1d0bd0218a3fecb3cc3ec9
Tomasz Kwiatkowski <tomasz.kwiatkowski@jitsolutions.pl>
2019-03-16 16:26:00 +0100

    3 commit

--- src/test/java/mohito/de/LoginTest.java
+++ src/test/java/mohito/de/LoginTest.java
@@ -2,6 +2,7 @@ package mohito.de;
 
 import static org.testng.Assert.assertTrue;
 
+import driver.RemoteDriverCreator;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -13,35 +14,41 @@ import mohito.de.testdata.UserDataProvider;
 import stores.mohito.commons.CommonTestSteps;
 import stores.mohito.page.PageObjectManager;
 
+import java.net.MalformedURLException;
+
 public class LoginTest {
 
-	private WebDriver driver;
-	private CommonTestSteps commonTestSteps;
-	private PageObjectManager manager;
-	private static final String URL = "http://mohito.com/de/de";
+    private WebDriver driver;
+    private CommonTestSteps commonTestSteps;
+    private PageObjectManager manager;
+    private static final String URL = "http://mohito.com/de/de";
 
-@Parameters("browser")
-@BeforeMethod
-	public void setUp(String browser) {
-		driver = new WebDriverCreator().createDriver(browser);
-		commonTestSteps = new CommonTestSteps(driver);
-		manager = new PageObjectManager(driver);
-	}
+    /*@Parameters("browser")
+        @BeforeMethod
+        public void setUp(String browser) throws MalformedURLException {
+                driver = new RemoteDriverCreator().createDriver(browser); */
+    @Parameters("browser")
+    @BeforeMethod
+    public void setUp(String browser) throws MalformedURLException {
+        driver = new RemoteDriverCreator().createDriver(browser);
+        commonTestSteps = new CommonTestSteps(driver);
+        manager = new PageObjectManager(driver);
+    }
 
-	@Test(dataProvider = "loginTest", dataProviderClass = UserDataProvider.class)
-	public void loginTest(String email, String password) {
+    @Test(dataProvider = "loginTest", dataProviderClass = UserDataProvider.class)
+    public void loginTest(String email, String password) {
 
-		commonTestSteps.loadMainPage(URL);
-		WebDriverCreator.setNewsletterCookie(driver);
-		commonTestSteps.loginProcess(email, password);
-		manager.getUserMenuPage().clickAccountIcon();
+        commonTestSteps.loadMainPage(URL);
+        WebDriverCreator.setNewsletterCookie(driver);
+        commonTestSteps.loginProcess(email, password);
+        manager.getUserMenuPage().clickAccountIcon();
 
-		assertTrue(manager.getUserAccountPage().isUserAccountPageDisplayed());
-	}
+        assertTrue(manager.getUserAccountPage().isUserAccountPageDisplayed());
+    }
 
-	@AfterMethod
-	public void tearDown() {
-		driver.quit();
-	}
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
 
 }
