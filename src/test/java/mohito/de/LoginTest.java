636b35c5fe674260c31d795d2e6d3889288797a4
Mateusz Kowalski <mateusz.kowalski@jit.team>
2020-11-07 12:59:48 +0100

    changed tests

--- src/test/java/mohito/de/LoginTest.java
+++ src/test/java/mohito/de/LoginTest.java
@@ -5,6 +5,7 @@ import static org.testng.Assert.assertTrue;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import driver.WebDriverCreator;
@@ -19,8 +20,10 @@ public class LoginTest {
 	private PageObjectManager manager;
 	private static final String URL = "http://mohito.com/de/de";
 
+	@Parameters("browser")
 	@BeforeMethod
-	public void setUp() {
+	public void setUp(String browser) {
+		driver = new WebDriverCreator().createDriver(browser);
 		driver = new WebDriverCreator().createDriver("chrome");
 		commonTestSteps = new CommonTestSteps(driver);
 		manager = new PageObjectManager(driver);
